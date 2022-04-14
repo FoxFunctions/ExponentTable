65f81843749fac0b20fb1eb64d6c0c7eb5f96881
Austin Howell <austinhowell@Austins-MacBook-Pro.local>
2022-04-14 08:01:16 -0400

    Added a method to close the program

--- Exponentstable/Program.cs
+++ Exponentstable/Program.cs
@@ -3,14 +3,14 @@
 namespace Exponentstable
 {
     public sealed class Program
-      {
-        static bool runAgain = true;
+    {
+        static bool userRunAgain = true;
         static int userInteger = 0;
 
         public static void Main()
         {
             Console.WriteLine("Welcome to the Exponent table.");
-            while (runAgain)
+            while (userRunAgain)
             {
                 while (true)
                 {
@@ -18,7 +18,7 @@ namespace Exponentstable
                     Console.WriteLine("Please enter an integer greater than zero");
                     Console.WriteLine();
                     int userInput = int.Parse(Console.ReadLine());
-                      if (userInput <= 0)
+                    if (userInput <= 0)
                     {
                         Console.WriteLine("That integer is less than or equal to 0. Please enter an integer that is greater than 0");
                         continue;
@@ -46,27 +46,27 @@ namespace Exponentstable
                 {
                     Console.WriteLine(i + "\t" + "\t" + Math.Pow(i, 2) + "\t" + "\t" + Math.Pow(i, 3));
                 }
-                while (true)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Would you like to run again? please enter y/n");
-                    string userPreference = Console.ReadLine().ToLower();
-                    if (userPreference == "y")
-                    {
-                        break;
-                    }
-                    else if (userPreference == "n")
-                    {
-                        Console.WriteLine("Goodbye");
-                        runAgain = false;
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("I'm sorry. I didn't quite get that.");
-                        continue;
-                    }
-                } 
+                userRunAgain = RunAgain();
+            }
+        }
+        public static bool RunAgain()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Would you like to run again? please enter y/n");
+            string userPreference = Console.ReadLine().ToLower();
+            if (userPreference == "y")
+            {
+                return true;
+            }
+            else if (userPreference == "n")
+            {
+                Console.WriteLine("Goodbye");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry. I didn't quite get that.");
+                return RunAgain();
             }
         }
     }
